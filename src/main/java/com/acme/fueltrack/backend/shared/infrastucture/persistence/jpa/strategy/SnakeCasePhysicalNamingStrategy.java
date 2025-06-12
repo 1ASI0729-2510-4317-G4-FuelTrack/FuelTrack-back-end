bbf6db8df2cee73469f73682fec8458502a29b3d
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-06-12 08:29:08 -0500

    initial commit

---
+++ src/main/java/com/acme/fueltrack/backend/shared/infrastucture/persistence/jpa/strategy/SnakeCasePhysicalNamingStrategy.java
@@ -0,0 +1,6 @@
+package com.acme.fueltrack.backend.shared.infrastucture.persistence.jpa.strategy;
+
+public class SnakeCasePhysicalNamingStrategy {
+}
+//implementar metodos
+//aqui se generan los metodos
