ab609edabb2c63c37873c37b6306792ce51863ae
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-07-08 14:55:47 -0500

    fix: fix route for connection

--- src/main/java/com/acme/fueltrack/backend/config/WebConfig.java
+++ src/main/java/com/acme/fueltrack/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los endpoints
-                .allowedOrigins("localhost:4200")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // si usas cookies o auth headers
